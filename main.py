5cd42d9bb5cfb486e6dca7ca1ba2a3b115c9e780
ZJ3N <gafergg123@gmail.com>
2026-09-08 18:54:58 +0300

    aa

--- main.py
+++ main.py
@@ -18,26 +18,6 @@ my_library.checkout_book("1111111111111")
 my_library.checkout_book("2222222222222")
 my_library.return_book("1111111111111")
 
-from library import Book, EBook, Library, is_valid_isbn
-
-my_library = Library()
-
-book1 = Book("FastAPI", "Bill Lubanovic", "1111111111111")
-book2 = Book("Python Crash Course", "Eric Matthes", "2222222222222")
-book3 = EBook("Designing Data-Intensive Applications", "Martin Kleppmann", "3333333333333", 12.99)
-book4 = EBook("Software Architecture: The Hard Parts", "Neal Ford Mark Richards Pramod Sadalage Zhamak Dehghani", "4444444444444", 20.0)
-book5 = Book("Clean Code", "Robert C. Martin", "5555555555555")
-
-my_library.add_book(book1)
-my_library.add_book(book2)
-my_library.add_book(book3)
-my_library.add_book(book4)
-my_library.add_book(book5)
-
-my_library.checkout_book("1111111111111")
-my_library.checkout_book("2222222222222")
-my_library.return_book("1111111111111")
-
 available_books = my_library.list_available_books()
 print("Available books:")
 for book in available_books:
